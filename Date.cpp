c7a17e484e158995ab3e534512cb5a5f949e4ee0
arcfernandes04 <arcfernandes04@gmail.com>
2024-08-12 23:31:30 +1200

    Added simple class for handling dates

---
+++ Date.cpp
@@ -0,0 +1,42 @@
+#include <ctime>
+#include "Date.h"
+
+using namespace std;
+
+Date::Date() {
+	time_t now = time(0);
+
+	struct tm date = *localtime(&now);
+
+	day = date.tm_mday;
+	month = date.tm_mon + 1;
+	year = date.tm_year + 1900;
+	wkDay = date.tm_wday + 1;
+	}
+
+Date::Date(int day, int month, int year, int wkDay) {
+	this->day = day;
+	this->month = month;
+	this->year = year;
+	this->wkDay = wkDay;
+}
+
+int Date::getDay() {
+	return day;
+}
+
+int Date::getMonth() {
+	return month;
+}
+
+int Date::getYear() {
+	return year;
+}
+
+int Date::getWkDay() {
+	return wkDay;
+}
+
+string Date::getWkDayString() {
+	return wkDays[wkDay - 1];
+}
---
+++ Date.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Date {
+private:
+	int day;
+	int month;
+	int year;
+	int wkDay;
+
+	vector<string> wkDays = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+public:
+	/*
+	 * Default constructor. Constructs a Date object with the current date of the system.
+	 */
+	Date();
+
+	/*
+	 * Constructor that takes in values to initialise fields.
+	 *
+	 * @param day The current day of the month (1 - 31).
+	 * @param month The current month of the year (1 - 12).
+	 * @param year The current year.
+	 * @param wkDay The currend day of the week (1 - 7, where 1 is Monday)
+	 */
+	Date(int day, int month, int year, int wkDay);
+
+	int getDay();
+
+	int getMonth();
+
+	int getYear();
+
+	int getWkDay();
+
+	string getWkDayString();
+};
