4efc5bb31152fb5db4b63cf0bbaebf5d06be009c
arcfernandes04 <arcfernandes04@gmail.com>
2024-08-14 12:08:24 +1200

    Completed Journal class definition and implementation

--- Hustle-Hamster/Journal.cpp
+++ Hustle-Hamster/Journal.cpp
@@ -1,10 +1,61 @@
 #include "Journal.h"
 
-
 Journal:: Journal() {
 	date = Date();
 }
 
 Journal::Journal(Date date) {
 	this->date = date;
+}
+
+Date Journal::getDate() {
+	return this->date;
+}
+
+int Journal::getDayRating() {
+	return this->dayRating;
+}
+
+int Journal::getSleepRating() {
+	return this->sleepRating;
+}
+
+string Journal::getMood() {
+	return this->mood;
+}
+
+vector<string> Journal::getActivities() {
+	return this->activities;
+}
+
+vector<string> Journal::getHammyThoughts() {
+	return this->hammyThoughts;
+}
+
+string Journal::getTextEntry() {
+	return this->textEntry;
+}
+
+void Journal::setDayRating(int dayRating) {
+	this->dayRating = dayRating;
+}
+
+void Journal::setSleepRating(int sleepRating) {
+	this->sleepRating = sleepRating;
+}
+
+void Journal::setMood(string mood) {
+	this->mood = mood;
+}
+
+void Journal::addActivity(string activity) {
+	this->activities.push_back(activity);
+}
+
+void Journal::addHammyThoughts(string hammyThought) {
+	this->hammyThoughts.push_back(hammyThought);
+}
+
+void Journal::setTextEntry(string textEntry) {
+	this->textEntry = textEntry;
 }
--- Hustle-Hamster/Journal.h
+++ Hustle-Hamster/Journal.h
@@ -7,7 +7,7 @@ private:
 	Date date;
 	int dayRating = 0;
 	int sleepRating = 0;
-	int moodRating = 0;
+	string mood = "";
 	vector<string> activities;
 	vector<string> hammyThoughts;
 	string textEntry = "";
@@ -21,32 +21,72 @@ public:
 	/**
 	* Construct a journal for the provided date.
 	* 
-	* @param date The date to construct the journal for
+	* @param date The date to construct the journal for.
 	*/
 	Journal(Date date);
 
+	/**
+	* Get the date of this journal.
+	*/
 	Date getDate();
 
+	/**
+	* Get the overall rating of this day.
+	*/
 	int getDayRating();
 
+	/**
+	* Get the rating for the sleep on this day. 
+	*/
 	int getSleepRating();
 
-	int getMoodRating();
+	/**
+	* Get the mood from this day.
+	*/
+	string getMood();
 
+	/**
+	* Get all of the activities completed on this day.
+	*/
 	vector<string> getActivities();
 
+	/**
+	* Get all of the thoughts provided by Hammy on this day.
+	*/
 	vector<string> getHammyThoughts();
 
+	/**
+	* Get the journal entry from this day. 
+	*/
 	string getTextEntry();
 
+	/**
+	* Set the overall rating for this day. 
+	*/
 	void setDayRating(int dayRating);
 
+	/**
+	* Set the sleep rating for this day.
+	*/
 	void setSleepRating(int sleepRating);
 
-	void getMoodRating(int moodRating);
+	/**
+	* Set the mood from this day. 
+	*/
+	void setMood(string mood);
 
+	/**
+	* Add an activity that was completed on this day. 
+	*/
 	void addActivity(string activity);
 
+	/**
+	* Add a thought shared by Hammy on this day. 
+	*/
+	void addHammyThoughts(string hammyThought);
 
-
+	/**
+	* Set the text entry for this day. 
+	*/
+	void setTextEntry(string textEntry);
 };
