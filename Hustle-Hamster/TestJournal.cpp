6750d6810b11b5dff32523e282d027306c2e0968
bekrol <rolre205@student.otago.ac.nz>
2024-08-19 07:04:35 +1200

    All Jounral unit tests pass - no comments yet

--- Hustle-Hamster/TestJournal.cpp
+++ Hustle-Hamster/TestJournal.cpp
@@ -7,47 +7,110 @@
 Journal journal = Journal();
 Date date = Date();
 
-int testDate() {
-    //journal = Journal(date);
-    return 0;
+void testDate() {
+    try
+    {
+        journal = Journal(date);
+        assert (journal.getDate().getDay() == date.getDay());
+        assert (journal.getDate().getMonth() == date.getMonth());
+        assert (journal.getDate().getYear() == date.getYear());
+        assert (journal.getDate().getWkDay() == date.getWkDay());
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << "Journal Date failed";
+    }
 }
 
 void testDayRating() {
-    journal.setDayRating(5);
-    assert (journal.getDayRating() == 5);
+    try
+    {
+        journal.setDayRating(5);
+        assert (journal.getDayRating() == 5);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << "Journal Day Rating failed";
+    }
+    
+
 }
 
 void testSleepRating() {
-    journal.setSleepRating(5);
-    assert (journal.getSleepRating() == 5);
+    try
+    {
+        journal.setSleepRating(5);
+        assert (journal.getSleepRating() == 5);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << "Journal Sleep Rating failed";
+    }
+    
 }
 
 void testMood() {
-    journal.setMood("Happy");
-    assert (journal.getMood() == "Happy");
+    try
+    {
+        journal.setMood("Happy");
+        assert (journal.getMood() == "Happy");
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << "Journal Mood failed";
+    }
+    
+
 }
 
 void testActivities() {
-    journal.addActivity("Activity 1");
-    journal.addActivity("Activity 2");
-    journal.addActivity("Activity 3");
-    assert (journal.getActivities().size() == 3);
-    assert (journal.getActivities()[0] == "Activity 1");
-    assert (journal.getActivities()[1] == "Activity 2");
-    assert (journal.getActivities()[2] == "Activity 3");
+    try
+    {
+        journal.addActivity("Activity 1");
+        journal.addActivity("Activity 2");
+        journal.addActivity("Activity 3");
+        assert (journal.getActivities().size() == 3);
+        assert (journal.getActivities()[0] == "Activity 1");
+        assert (journal.getActivities()[1] == "Activity 2");
+        assert (journal.getActivities()[2] == "Activity 3");
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << "Journal Activities failed";
+    }
+    
+
 }
 
 void testHammyThoughts() {
-    journal.addHammyThoughts("This is a test thought");
-    journal.addHammyThoughts("This is another test thought");
-    assert (journal.getHammyThoughts().size() == 2);
-    assert (journal.getHammyThoughts()[0] == "This is a test thought");
-    assert (journal.getHammyThoughts()[1] == "This is another test thought");
+    try
+    {
+        journal.addHammyThoughts("This is a test thought");
+        journal.addHammyThoughts("This is another test thought");
+        assert (journal.getHammyThoughts().size() == 2);
+        assert (journal.getHammyThoughts()[0] == "This is a test thought");
+        assert (journal.getHammyThoughts()[1] == "This is another test thought");
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << "Journal Hammy Thoughts failed";
+    }
+    
+    
 }
 
 void testTextEntry() {
-    journal.setTextEntry("This is a test entry");
-    assert (journal.getTextEntry() == "This is a test entry");
+    try
+    {
+        journal.setTextEntry("This is a test entry");
+        assert (journal.getTextEntry() == "This is a test entry");
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << "Journal Text Entry failed";
+    }
+    
+
 }
 
 int main() {
