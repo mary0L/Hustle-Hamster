607c623bbac9a9b6dee828b4adbd1ac491ace785
bekrol <rolre205@student.otago.ac.nz>
2024-08-29 16:30:17 +1200

    Removed utils testing for now

--- Testing/utils-Test.cpp
+++ Testing/utils-Test.cpp
@@ -8,7 +8,6 @@
 
 void testPrintReport(Journal &journal)
 {
-    
     // Get the output from the printReport function
     std::stringstream output;
     std::streambuf *coutbuf = std::cout.rdbuf(); 
@@ -22,17 +21,17 @@ void testPrintReport(Journal &journal)
 
     // Check the output
     std::stringstream expectedOutput;
-    expectedOutput << "You rated your day a 4/5\n";
-    expectedOutput << "You rated your sleep a 3/5\n";
-    expectedOutput << "You said your mood today was: Good\n";
-    expectedOutput << "----------------------------------------------------------\n";
-    expectedOutput << "Here are the activities you completed today:\n";
-    expectedOutput << "Activity 1\n";
-    expectedOutput << "Activity 2\n";
-    expectedOutput << "----------------------------------------------------------\n";
-    expectedOutput << "Your thoughts on the day:\n";
-    expectedOutput << "This is a test entry\n";
-    expectedOutput << "----------------------------------------------------------\n";
+    expectedOutput << "You rated your day a 5/5";
+    expectedOutput << "You rated your sleep a 3/5";
+    expectedOutput << "You said your mood today was: Sad";
+    expectedOutput << "----------------------------------------------------------";
+    expectedOutput << "Here are the activities you completed today:";
+    expectedOutput << "Activity 1";
+    expectedOutput << "Activity 2";
+    expectedOutput << "----------------------------------------------------------";
+    expectedOutput << "Your thoughts on the day:";
+    expectedOutput << "This is a test entry";
+    expectedOutput << "----------------------------------------------------------";
 
     assert(output.str() == expectedOutput.str());
 }
@@ -53,7 +52,7 @@ int main()
     // Run tests
     testPrintReport(journal);
 
-    // Delete the journal object
+    // Tear down - Delete the journal object
     journal.~Journal();
 
     std::cout << "All tests passed!" << std::endl;
