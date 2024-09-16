0ce48a2129edaf046ea9412ff387dd897e76f409
MaryO <maryoleary500@gmail.com>
2024-09-16 13:35:53 +1200

    start implementing logic for checking activities (WIP - activities not present)

--- Hustle-Hamster/feedback.cpp
+++ Hustle-Hamster/feedback.cpp
@@ -2,17 +2,41 @@
 
 
 void checkAdvice(Journal& dailyEntry) {
-	//first check if the sleep was in the 1 or 2 (bad)
-	//assign the activities to an array and cycle through each one to see what they didn't complete
-	//depending on activity go to that specific method
+	vector<string> activities = dailyEntry.getActivities();
+	vector<string> incompleteActivities;
+
+	if (dailyEntry.getSleepRating() < 3) {
+		incompleteActivities.push_back("bad sleep");
+		TYPE("You didn't have a great sleep today");
+		TYPE(incompleteActivities.back());
+	}
+
+	for (string activity : activities)
+	{
+		TYPE("activity");
+		TYPE(activity);
+		if (dailyEntry.didActivity(activity)) {
+			incompleteActivities.push_back(activity);
+		}
+
+	}
+
+	TYPE("You didn't complete these activities");
+	for (string a : incompleteActivities) {
+		TYPE(a);
+	}
+
 }
 
 void hammyEvaluation(Journal& dailyEntry) {
 	printHammy();
 	//check what the mood rating is, if less than three do a comprehensive feedback, if greater than three be encouraging
 	if (dailyEntry.getDayRating() <= 3) {
-		TYPE("You have rated your day less than three");
-		TYPE("Let's see the activities you have completed today");
+		TYPE("You had a low day rating today...lets have a look at some things that might help!");
 		checkAdvice(dailyEntry);
+	}
+	else {
+		TYPE("I see you're feeling positive today, this is great!");
+		//other stuff idk what to say
 	}
 }
