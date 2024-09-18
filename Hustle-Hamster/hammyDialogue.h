858b2405de48ded0ed4985469220658f75cb2387
“winka826” <winka826@student.otago.ac.nz>
2024-09-19 00:18:21 +1200

    comments added, fixed CMake silly mistake

--- Hustle-Hamster/hammyDialogue.h
+++ Hustle-Hamster/hammyDialogue.h
@@ -9,25 +9,22 @@
 #pragma once
 
 #include <vector>
-#include <stdio.h>
-#include <stdlib.h>
-#include <iostream>
-#include <cctype> 
-#include <sstream>
-#include <vector>
 #include "utils.h"
-#include <fstream>
-#include <limits>
-#include <random>
 
+/* Vector with all responses to user selecting daily log */
 extern vector<string> dailyLogR;
 
+/* Vector with all questions for daily rating */
 extern vector<string> dailyRatingQ;
 
+/* Vector with all questions for sleep rating */
 extern vector<string> sleepRatingQ;
 
+/* Vector with all questions for mood */
 extern vector<string> moodQ;
 
+/* Vector with all responses to user selecting exit */
 extern vector<string> exitResponses;
 
+/* Rating system for day rating and sleep rating */
 extern string ratingSystem;
