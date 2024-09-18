27b2e5d289064fdc2b6dea5308099ce1c89b7206
“winka826” <winka826@student.otago.ac.nz>
2024-09-18 22:48:28 +1200

    WIP dialigue

---
+++ Hustle-Hamster/hammyDialogue.cpp
@@ -0,0 +1,14 @@
+#include "hammyDialogue.h"
+
+std::vector<std::string> dailyRating = {
+    "Awesome, let’s see how your day has been!",
+    "Great! Let’s get started!",
+    "Let’s get logging!",
+    "Let’s see how awesome your day has been!"
+};
+
+std::vector<std::string> sleepLog = {
+    "How did you sleep?",
+    "How was thou slumber?", 
+    "Did you sleep well?"
+};
---
+++ Hustle-Hamster/hammyDialogue.h
@@ -0,0 +1,14 @@
+/**
+ * @file hammyDialogue.h
+ * @author Katie Wink
+ *
+ * @brief File containing different hammy dialogue and responses
+ *
+ */
+
+#pragma once
+
+#include <vector>
+
+
+
